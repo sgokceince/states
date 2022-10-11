49bdbf2e2898444b13863619a72326af20932582
sunay gokce ince <sgokceince@icloud.com>
2022-10-11 16:41:15 +0300

    setAddressi obje olarak tanımladık

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [name, setName] = useState("Sakiz");
   const [age, setAge] = useState(3);
   const [friends, setFriends] = useState(["Sunay", "Orhan"]);
+  const [address, setAddress] = useState({title: "Mersin", zip: "33400"});
 
   return (
     <div className="App">
@@ -24,7 +25,20 @@ function App() {
 
        <br />
        <button onClick={() => setFriends([...friends, "Sümbül"])}>Add new friend</button>
+       
+       <hr />
+       <br></br>
 
+       <h2>Address</h2>
+       <div>
+
+         {address.title} {address.zip}
+
+       </div>
+
+       <br />
+
+       <button onClick={() => setAddress({...address, title: "İstanbul", zip: "34400"})}>Change the Address</button>
     </div>
   );
 }
