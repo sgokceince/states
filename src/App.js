99fb41b03af380f35db6aba2d750009876e0e1df
sunay gokce ince <sgokceince@icloud.com>
2022-10-11 14:48:35 +0300

    array olarak useState tanımlandı ve map fonksiyonu ile yazdırıldı.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ function App() {
 
   const [name, setName] = useState("Sakiz");
   const [age, setAge] = useState(3);
+  const [friends, setFriends] = useState(["Sunay", "Orhan"]);
+
   return (
     <div className="App">
-     <div>Merhaba {name}</div>
-     <h1>{age}</h1>
-     <button onClick={() => setName("Gusta")}>change name</button>
-     <button onClick={() => setAge(5)}>change age</button>
+       <div>Merhaba {name}</div>
+       <h1>{age}</h1>
+       <button onClick={() => setName("Gusta")}>change name</button>
+       <button onClick={() => setAge(5)}>change age</button>
+       <hr />
+
+       {
+         friends.map(friend => <div>{friend}</div>)
+       }
+
     </div>
   );
 }
