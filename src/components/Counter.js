d42c7af0bb03fc5d838bca1064e8fee9ddcc4b43
sunay gokce ince <sgokceince@icloud.com>
2022-10-14 15:09:11 +0300

    increase değişkenini sabite atayıp onclick metodu ile çagırdık

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,11 +2,15 @@ import { useState } from 'react'
 
 function Counter() {
   const [count, setCount] = useState(0);
+
+  const increase = () => {
+      setCount(count + 1)
+  }
   return (
     <div>
         <h1>{ count }</h1>
-        <button>Decrease</button>
-        <button>Increase</button>
+        <button onClick={() => setCount(count - 1)}>Decrease</button>
+        <button onClick={increase }>Increase</button>
     </div>
   )
 }
